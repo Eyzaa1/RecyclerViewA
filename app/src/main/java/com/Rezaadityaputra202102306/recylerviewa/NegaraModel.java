1dd45e8ef3c229742fd33720bf7205da4244b9ce
Eyzaa1 <93369160+Eyzaa1@users.noreply.github.com>
2023-06-13 10:21:27 +0700

    Initial commit

---
+++ app/src/main/java/com/Rezaadityaputra202102306/recylerviewa/NegaraModel.java
@@ -0,0 +1,33 @@
+package com.Rezaadityaputra202102306.recylerviewa;
+
+public class NegaraModel {
+    private String nama, dekripsi,url;
+    public NegaraModel()
+    {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDekripsi() {
+        return dekripsi;
+    }
+
+    public void setDekripsi(String dekripsi) {
+        this.dekripsi = dekripsi;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+
+    }
+}
